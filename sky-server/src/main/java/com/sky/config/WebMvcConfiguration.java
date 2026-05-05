995488d201c139364c7ec6bdc61a6834d3463417
djsl14 <2295026317@qq.com>
2026-05-05 17:24:28 +0800

    购物车查看与新增

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -46,9 +46,9 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                 .addPathPatterns("/admin/**")
                 .excludePathPatterns("/admin/employee/login");
         registry.addInterceptor(jwtTokenUserInterceptor)
-                .addPathPatterns("/User/**")
-                .excludePathPatterns("/User/login")
-                .excludePathPatterns("/User/shop/status");
+                .addPathPatterns("/user/**")
+                .excludePathPatterns("/user/user/login")
+                .excludePathPatterns("/user/shop/status");
     }
 
     /**
